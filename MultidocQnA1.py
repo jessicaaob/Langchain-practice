553e445ee1f60251cda2a89e734e812d61c221a0
jessicaaob <89574789+jessicaaob@users.noreply.github.com>
2023-07-01 21:19:45 +0100

    Update MultidocQnA1.py

--- MultidocQnA1.py
+++ MultidocQnA1.py
@@ -8,19 +8,19 @@ from langchain.chains import RetrievalQAWithSourcesChain
 import PyPDF2
 
 #Function goes through pdfs and extracts and returns a list of all combined text and a list of combined sources 
-# def read_and_textify(files):
-#     text_list = []
-#     sources_list = []
-#     for file in files:
-#         pdfReader = PyPDF2.PdfReader(file)
-#         #print("Page Number:", len(pdfReader.pages))
-#         for i in range(len(pdfReader.pages)):
-#           pageObj = pdfReader.pages[i]
-#           text = pageObj.extract_text()
-#           pageObj.clear()
-#           text_list.append(text)
-#           sources_list.append(file.name + "_page_"+str(i))
-#     return [text_list,sources_list]
+def read_and_textify(files):
+    text_list = []
+    sources_list = []
+    for file in files:
+        pdfReader = PyPDF2.PdfReader(file)
+        #print("Page Number:", len(pdfReader.pages))
+        for i in range(len(pdfReader.pages)):
+          pageObj = pdfReader.pages[i]
+          text = pageObj.extract_text()
+          pageObj.clear()
+          text_list.append(text)
+          sources_list.append(file.name + "_page_"+str(i))
+    return [text_list,sources_list]
 
 # Some streamlit app set up/ configuration
 st.set_page_config(layout="centered", page_title="ESB_Help")
@@ -38,26 +38,22 @@ elif uploaded_files:
   st.write(str(len(uploaded_files)) + " document(s) loaded..")
 
 # use pdf function from above to read th euploaded pdfs and output the text and sources lists
-  # textify_output = read_and_textify(uploaded_files)
-
-  text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=100)
-  docs = text_splitter.split_documents(uploaded_files)
-
-  # documents = textify_output[0]
-  # sources = textify_output[1]
+  textify_output = read_and_textify(uploaded_files)
+  
+  documents = textify_output[0]
+  sources = textify_output[1]
 
   # model set up  
   #extract embeddings
   embeddings = OpenAIEmbeddings(openai_api_key = st.secrets["openai_api_key"])
   #vectore with metadata. Here we will store page numbers.
-
-  vStore = Chroma.from_texts(docs, embeddings)#, metadatas=[{"source": s} for s in sources])
+  vStore = Chroma.from_texts(documents, embeddings, metadatas=[{"source": s} for s in sources])
   #pick a model
   model_name = "gpt-3.5-turbo"
   # retriver and number of docs to be used 
     ### could improve this part, character output limits erroring here) ###
   retriever = vStore.as_retriever()
-  retriever.search_kwargs = {'k':4}
+  retriever.search_kwargs = {'k':3}
 
   # initiate model
   llm = OpenAI(model_name=model_name, openai_api_key = st.secrets["openai_api_key"], streaming=True)
@@ -74,8 +70,8 @@ elif uploaded_files:
         result = model({"question":user_q}, return_only_outputs=True)
         st.subheader('Your response:')
         st.write(result['answer'])
-        #st.subheader('Source pages:')
-        #st.write(result['sources'])
+        st.subheader('Source pages:')
+        st.write(result['sources'])
     except Exception as e:
       st.error(f"An error occurred: {e}")
       st.error('Oops, the GPT response resulted in an error :( Please try again with a different question.')
